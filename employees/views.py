668d05257a376a8e485d22f7be227da928e47f32
Ajay Joy <developer.ajay618@gmail.com>
2024-06-12 19:14:55 +0530

    employee list page

--- employees/views.py
+++ employees/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 
+from employees.models import Employee
+
 # Create your views here.
 
 def home(request):
-    return render(request , 'home.html')
+    employees  = Employee.objects.all()
+    context = {
+        'employees' : employees
+    }
+    return render(request , 'home.html' ,context)
